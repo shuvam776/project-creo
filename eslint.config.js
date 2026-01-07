e6c1ab1a9f1f37ad900e3d2fc4567b26dedaf3fd
ayush <ayushatschool5@gmail.com>
2026-01-08 00:52:43 +0530

    fix: build

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,10 @@ export default defineConfig([
     rules: {
       'react-refresh/only-export-components': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
   },
 ])
