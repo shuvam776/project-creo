8906107b785dfd458fc03c308d3367f30c7db0e7
Ayush <135319056+ayussh-2@users.noreply.github.com>
2026-01-11 00:34:20 +0530

    Add Hero component to PlaygroundPage

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,3 +1,4 @@
+import Hero from '@/components/hero/Hero'
 import Footer from '@/components/footer/Footer'
 import PlaygroundContent from '@/components/ui/playground'
 
@@ -5,6 +6,7 @@ export default function Playground() {
   return (
     <main className='w-full overflow-x-hidden'>
       <PlaygroundContent />
+      <Hero />
       <Footer />
     </main>
   )
